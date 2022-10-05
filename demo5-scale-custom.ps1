cde8e844203338a852143c7c0625b53764f99d93
Juan Ramírez <jramirez@plainconcepts.com>
2022-10-05 20:09:56 +0200

    update main

--- demo5-scale-custom.ps1
+++ demo5-scale-custom.ps1
@@ -9,6 +9,15 @@ $encodedMessage = [Convert]::ToBase64String($bytes)
 1..20 | ForEach-Object -Parallel {
     Write-Host "Send message..."
 
-   
+    $storageConnectionString  = $($using:storageConnectionString)
+    $storageAccount  = $($using:storageAccount)
+    $queue  = $($using:queue)
+    $encodedMessage  = $($using:encodedMessage)
+
+    az storage message put `
+                --connection-string $storageConnectionString `
+                --account-name $storageAccount `
+                --queue-name $queue `
+                --content $encodedMessage `
 
 } -ThrottleLimit 10
