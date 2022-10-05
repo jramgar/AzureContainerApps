094d4e48f5d53bbc1ff7442207581457f360782c
Carlos Recuero Díaz <crecuero@plainconcepts.com>
2022-10-05 17:45:44 +0200

    Clean Claims table

--- src/Frontend/Pages/Index.cshtml.cs
+++ src/Frontend/Pages/Index.cshtml.cs
@@ -3,29 +3,37 @@ using Frontend.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
-using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace Frontend.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly BackendOptions _backendOptions;
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory, IOptions<BackendOptions> backendOptions)
+        //private readonly ILogger<IndexModel> _logger;
+        //private readonly IHttpClientFactory _httpClientFactory;
+        //private readonly BackendOptions _backendOptions;
+        //public IndexModel(ILogger<IndexModel> logger) //, IHttpClientFactory httpClientFactory, IOptions<BackendOptions> backendOptions)
+        //{
+        //    //_logger = logger;
+        //    //_httpClientFactory = httpClientFactory;
+        //    //_backendOptions = backendOptions.Value;
+        //}
+
+        public IActionResult OnGet()
         {
-            _logger = logger;
-            _httpClientFactory = httpClientFactory;
-            _backendOptions = backendOptions.Value;
+            //var httpClient = _httpClientFactory.CreateClient();
+            //var url = $"{_backendOptions.BaseUrl}/weatherforecast";
+            //var weatherForecast = await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>(url);
+            //ViewData["weatherForecast"] = weatherForecast;
+            //return Page();
+            ViewData["Claims"] = GetClaims();
+            return Page();
         }
 
-        //public async Task<IActionResult> OnGetAsync()
-        //{
-        //    var httpClient = _httpClientFactory.CreateClient();
-        //    var url = $"{_backendOptions.BaseUrl}/weatherforecast";
-        //    var weatherForecast = await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>(url);
-        //    ViewData["weatherForecast"] = weatherForecast;
-        //    return Page();
-        //}
+        private IEnumerable<Claim> GetClaims()
+        {
+            var user = HttpContext.User;
+            return user.Claims;
+        }
     }
 }
